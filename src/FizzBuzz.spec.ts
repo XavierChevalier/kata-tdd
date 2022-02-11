df3e8a2780c9cbb5878e1f9963cef5ae3b5717c7
XavierChevalier <mcmodz@hotmail.fr>
2022-02-11 10:08:01 +0100

    feat(fizz-buzz): return "Buzz" with 5

--- src/FizzBuzz.spec.ts
+++ src/FizzBuzz.spec.ts
@@ -9,7 +9,15 @@ describe('FizzBuzz', () => {
     expect(fizzBuzz(2)).toBe(2);
   });
 
-  it('should returns 3 with 3', () => {
+  it('should returns "Fizz" with 3', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it('should returns 4 with 4', () => {
+    expect(fizzBuzz(4)).toBe(4);
+  });
+
+  it('should returns "Buzz" with 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
   });
 });
--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,5 +1,9 @@
 // Red - Green - Refactoring
 export function fizzBuzz(number: number) {
+  if (number === 5) {
+    return 'Buzz';
+  }
+
   if (number === 3) {
     return 'Fizz';
   }
