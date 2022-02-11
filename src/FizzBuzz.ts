973dc45405180d2ee649cecacdda9a4fcdec9e9a
XavierChevalier <mcmodz@hotmail.fr>
2022-02-11 10:39:00 +0100

    refactor(fizz-buzz): create some utils functions

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -2,14 +2,24 @@ const BUZZ = 'Buzz';
 const FIZZ = 'Fizz';
 const FIZZ_BUZZ = 'FizzBuzz';
 const BuzzDivider = 5;
-const FizzDivider = 3;
+
+function isDivisibleBy(number: number, divider: number) {
+  return number % divider === 0;
+}
+
+function isDivisibleByFive(number: number) {
+  return isDivisibleBy(number, 5);
+}
+
+function isDivisibleByThree(number: number) {
+  return isDivisibleBy(number, 3);
+}
 
 // Red - Green - Refactoring
 export function fizzBuzz(number: number) {
-  if (number % FizzDivider === 0 && number % BuzzDivider === 0)
-    return FIZZ_BUZZ;
+  if (isDivisibleByThree(number) && isDivisibleByFive(number)) return FIZZ_BUZZ;
   if (number === BuzzDivider) return BUZZ;
-  if (number % FizzDivider === 0) return FIZZ;
+  if (isDivisibleByThree(number)) return FIZZ;
 
   return number;
 }
