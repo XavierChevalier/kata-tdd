8b15d80d7cf29a041692a92eaa0872b8d2381f54
XavierChevalier <mcmodz@hotmail.fr>
2022-02-11 08:50:01 +0100

    feat: initialize project

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!**/*.d.ts'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.ts', '!**/*.d.ts', '!**/*.spec.ts'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
 };
