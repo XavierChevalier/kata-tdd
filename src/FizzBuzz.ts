ff0719d43933c1370fbc6ce6c39f7cd4db6e61fa
XavierChevalier <mcmodz@hotmail.fr>
2022-02-11 10:17:39 +0100

    refactor(fizz-buzz): reduce the number of conditions

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -6,8 +6,7 @@ const FizzDivider = 3;
 // Red - Green - Refactoring
 export function fizzBuzz(number: number) {
   if (number === BuzzDivider) return BUZZ;
-  if (number === FizzDivider) return FIZZ;
-  if (number === 6) return FIZZ;
+  if (number % FizzDivider === 0) return FIZZ;
 
   return number;
 }
