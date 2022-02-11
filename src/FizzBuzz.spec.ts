312dfe00875a03f502368f714bd05e72e9a4de23
XavierChevalier <mcmodz@hotmail.fr>
2022-02-11 09:58:06 +0100

    feat(fizz-buzz): returns 2 with 2

--- src/FizzBuzz.spec.ts
+++ src/FizzBuzz.spec.ts
@@ -4,4 +4,8 @@ describe('FizzBuzz', () => {
   it('should returns 1 with 1', () => {
     expect(fizzBuzz(1)).toBe(1);
   });
+
+  it('should returns 2 with 2', () => {
+    expect(fizzBuzz(2)).toBe(2);
+  });
 });
--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,3 +1,7 @@
 export function fizzBuzz(number: number) {
+  if (number === 2) {
+    return 2;
+  }
+
   return 1;
 }
